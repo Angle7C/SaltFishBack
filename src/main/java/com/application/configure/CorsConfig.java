053c6b603ab653651816c042fcb8b14fea7c8eb7
chen <15305067103@163.com>
2022-06-09 10:06:35 +0800

    添加了跨域解决方案

---
+++ src/main/java/com/application/configure/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.application.configure;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * AJAX请求跨域
+ * @author Mr.W
+ * @time 2018-08-13
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    static final String ORIGINS[] = new String[]{"GET", "POST", "PUT", "DELETE"};
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:80","http://localhost:8080","https://gitee.com")
+                .allowCredentials(true)
+                .allowedMethods(ORIGINS)
+                .exposedHeaders("**")
+                .maxAge(3600);
+    }
+}
