88cf794ec60ad3141b740b9ffec1f716763e3e44
chen <15305067103@163.com>
2022-06-16 10:04:13 +0800

    题解查询

--- src/main/java/com/application/model/entity/Comment.java
+++ src/main/java/com/application/model/entity/Comment.java
@@ -1,9 +1,12 @@
 package com.application.model.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-
+@AllArgsConstructor
+@NoArgsConstructor
 public class Comment {
     private Long id;
 
