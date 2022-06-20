01443ef2eb04bd27ebd09994e64d67e6ff689de3
chen <15305067103@163.com>
2022-06-20 16:16:46 +0800

    题解点赞

---
+++ src/main/java/com/application/mapper/LikesMapper.java
@@ -0,0 +1,32 @@
+package com.application.mapper;
+
+import com.application.model.entity.Likes;
+import com.application.model.entity.LikesExample;
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+@Mapper
+public interface LikesMapper {
+    int countByExample(LikesExample example);
+
+    int deleteByExample(LikesExample example);
+
+    int deleteByPrimaryKey(Long userId);
+
+    int insert(Likes record);
+
+    int insertSelective(Likes record);
+
+    List<Likes> selectByExample(LikesExample example);
+
+    Likes selectByPrimaryKey(Long userId);
+
+    int updateByExampleSelective(@Param("record") Likes record, @Param("example") LikesExample example);
+
+    int updateByExample(@Param("record") Likes record, @Param("example") LikesExample example);
+
+    int updateByPrimaryKeySelective(Likes record);
+
+    int updateByPrimaryKey(Likes record);
+}
---
+++ src/main/java/com/application/model/entity/Likes.java
@@ -0,0 +1,40 @@
+package com.application.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Likes {
+    private Long userId;
+
+    private Long commentId;
+
+    private Long id;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getCommentId() {
+        return commentId;
+    }
+
+    public void setCommentId(Long commentId) {
+        this.commentId = commentId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
